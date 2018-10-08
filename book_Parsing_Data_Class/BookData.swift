f0e40a3a27011cc76a0b904cb5aabc1b8d8a9570
D7703_16 <d7703_16@d7703-16ui-imac.local>
2018-10-08 11:16:38 +0900

    Class Parsing

---
+++ book_Parsing_Data_Class/BookData.swift
@@ -0,0 +1,13 @@
+//
+//  BookData.swift
+//  book_Parsing_Data_Class
+//
+//  Created by D7703_16 on 2018. 10. 8..
+//  Copyright © 2018년 201524024. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
